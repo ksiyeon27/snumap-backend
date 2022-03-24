14515a246ef4dd13fe32c6a397080075d96b2141
esc5221 <esc5221@gmail.com>
2022-03-24 22:26:40 +0900

    Fix: mysql 연결, url 뒤에 / 추가

--- snumap/user/urls.py
+++ snumap/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import UserSignUpView, UserLoginView, UserLogoutView
 
 urlpatterns = [
-    path('signup', UserSignUpView.as_view(), name='signup'),    # /signup
-    path('login', UserLoginView.as_view(), name='login'),       # /login
-    path('logout', UserLogoutView.as_view(), name='logout'),    # /logout
+    path('signup/', UserSignUpView.as_view(), name='signup'),    # /signup
+    path('login/', UserLoginView.as_view(), name='login'),       # /login
+    path('logout/', UserLogoutView.as_view(), name='logout'),    # /logout
 ]
