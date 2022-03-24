0614345a784cc004c32abac463de4ea0b2da0d94
ksiyeon27 <siyeonk27@gmail.com>
2022-03-24 20:28:39 +0900

    Fix: add logout url

--- snumap/user/urls.py
+++ snumap/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserSignUpView, UserLoginView
+from .views import UserSignUpView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('signup', UserSignUpView.as_view(), name='signup'),    # /signup
     path('login', UserLoginView.as_view(), name='login'),       # /login
+    path('logout', UserLogoutView.as_view(), name='logout'),    # /logout
 ]
