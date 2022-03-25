4c4ea6dc94dd3a3a5b3fdea2fe9f6afd1890a383
EC2 Default User <ec2-user@ip-172-31-2-64.us-east-2.compute.internal>
2022-03-25 08:57:21 +0000

    Feat: docker 배포 설정 완료

--- snumap/snumap/settings.py
+++ snumap/snumap/settings.py
@@ -31,9 +31,9 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "django"]
 
 
 # Application definition
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
